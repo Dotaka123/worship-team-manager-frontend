14e831fe972915eac62249c8ac18060fd5768b02
Tata <142669125+Dotaka123@users.noreply.github.com>
2026-02-09 16:23:09 +0300

    Add files via upload

---
+++ src/components/Layout-improved.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
